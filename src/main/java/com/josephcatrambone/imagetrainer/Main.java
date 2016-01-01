a413ab426ed56c017bfa30b97585e9ad009e80e5
Joseph Catrambone <jo.jcat@gmail.com>
2015-12-31 19:34:03 -0800

    Refactor into build/train functions.

--- src/main/java/com/josephcatrambone/imagetrainer/Main.java
+++ src/main/java/com/josephcatrambone/imagetrainer/Main.java
@@ -5,6 +5,7 @@ import com.josephcatrambone.aij.networks.*;
 import com.josephcatrambone.aij.trainers.BackpropTrainer;
 import com.josephcatrambone.aij.trainers.ContrastiveDivergenceTrainer;
 import com.josephcatrambone.aij.trainers.ConvolutionalTrainer;
+import com.josephcatrambone.aij.trainers.DeepNetworkTrainer;
 import com.josephcatrambone.aij.utilities.ImageTools;
 import com.josephcatrambone.aij.utilities.NetworkIOTools;
 import javafx.animation.KeyFrame;
@@ -33,18 +34,19 @@ import java.util.logging.Logger;
  * Created by Jo on 10/31/2015.
  */
 public class Main extends Application {
-	final int IMAGE_WIDTH = 256;
-	final int IMAGE_HEIGHT = 256;
+	final int IMAGE_WIDTH = 32;
+	final int IMAGE_HEIGHT = 32;
+
+	final String IMAGE_PATH = "./images";
+	final String POSITIVE_EXAMPLE_PATH = "./positive_examples";
+	final String NEGATIVE_EXAMPLE_PATH = "./negative_examples";
+	final String IMAGE_EXTENSION = ".png";
+	final int IMAGE_LIMIT = 4000;
 
 	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-	private Network buildNetwork() {
-		final String IMAGE_PATH = "./images";
-		final String POSITIVE_EXAMPLE_PATH = "./positive_examples";
-		final String NEGATIVE_EXAMPLE_PATH = "./negative_examples";
-		final String IMAGE_EXTENSION = ".png";
+	private DeepNetwork buildNetwork() {
 		final String FINAL_NETWORK_FILENAME = "dn.dat";
-		final int IMAGE_LIMIT = 4000;
 		final int STEP_0 = 1;
 		final int STEP_1 = 16;
 		final int STEP_2 = 16;
@@ -54,7 +56,7 @@ public class Main extends Application {
 		// If we already trained our network, just load it and return.
 		Network trainedNetwork = NetworkIOTools.loadNetworkFromDisk(FINAL_NETWORK_FILENAME);
 		if(trainedNetwork != null) {
-			return trainedNetwork;
+			return (DeepNetwork)trainedNetwork;
 		}
 
 		// Set up our network!
@@ -68,6 +70,7 @@ public class Main extends Application {
 				RBM_OUTPUT, RBM_OUTPUT,
 				STEP_0, STEP_0,
 				ConvolutionalNetwork.EdgeBehavior.ZEROS);
+
 		final MaxPoolingNetwork mp0 = new MaxPoolingNetwork(RBM_OUTPUT*RBM_OUTPUT*4);
 		final ConvolutionalNetwork conv1 = new ConvolutionalNetwork(mp0,
 				conv0.getOutputWidth(), conv0.getOutputHeight(),
@@ -75,6 +78,7 @@ public class Main extends Application {
 				1, 1,
 				RBM_OUTPUT, RBM_OUTPUT, // Since each step maxpools two RBM outputs, step sideways this much.
 				ConvolutionalNetwork.EdgeBehavior.ZEROS);
+
 		final RestrictedBoltzmannMachine rbm1 = new RestrictedBoltzmannMachine(RBM_INPUT*RBM_INPUT, RBM_OUTPUT*RBM_OUTPUT);
 		final ConvolutionalNetwork conv2 = new ConvolutionalNetwork(rbm1,
 				conv1.getOutputWidth(), conv1.getOutputHeight(),
@@ -82,6 +86,7 @@ public class Main extends Application {
 				RBM_OUTPUT, RBM_OUTPUT,
 				STEP_2, STEP_2,
 				ConvolutionalNetwork.EdgeBehavior.ZEROS);
+
 		final MaxPoolingNetwork mp1 = new MaxPoolingNetwork(RBM_OUTPUT*RBM_OUTPUT*4);
 		final ConvolutionalNetwork conv3 = new ConvolutionalNetwork(mp1,
 				conv2.getOutputWidth(), conv2.getOutputHeight(),
@@ -89,6 +94,7 @@ public class Main extends Application {
 				1, 1,
 				RBM_OUTPUT, RBM_OUTPUT,
 				ConvolutionalNetwork.EdgeBehavior.ZEROS);
+
 		final RestrictedBoltzmannMachine rbm2 = new RestrictedBoltzmannMachine(RBM_INPUT*RBM_INPUT, RBM_OUTPUT*RBM_OUTPUT);
 		final ConvolutionalNetwork conv4 = new ConvolutionalNetwork(rbm2,
 				conv3.getOutputWidth(), conv3.getOutputHeight(),
@@ -96,7 +102,56 @@ public class Main extends Application {
 				RBM_OUTPUT, RBM_OUTPUT,
 				STEP_1, STEP_1,
 				ConvolutionalNetwork.EdgeBehavior.ZEROS);
+
 		final NeuralNetwork fc = new NeuralNetwork(new int[] {conv4.getNumOutputs(), 100, 2}, new String[] {"tanh", "tanh", "tanh"});
+
+		DeepNetwork dn = new DeepNetwork(conv0, conv1, conv2, conv3, conv4);
+		NetworkIOTools.saveNetworkToDisk(dn, FINAL_NETWORK_FILENAME);
+
+		return dn;
+	}
+
+	private DeepNetworkTrainer buildTrainer(DeepNetwork net) {
+		DeepNetworkTrainer dnt = new DeepNetworkTrainer(net);
+
+		ContrastiveDivergenceTrainer trainer0_0 = new ContrastiveDivergenceTrainer();
+		ConvolutionalTrainer trainer0_1 = new ConvolutionalTrainer();
+		trainer0_1.operatorTrainer = trainer0_0;
+		dnt.setTrainer(0, trainer0_1); // Train layer zero convolution with ct.
+
+		dnt.setTrainer(1, null); // No training for maxpool.
+
+		ContrastiveDivergenceTrainer trainer2_0 = new ContrastiveDivergenceTrainer();
+		ConvolutionalTrainer trainer2_1 = new ConvolutionalTrainer();
+		trainer0_1.operatorTrainer = trainer2_0;
+		dnt.setTrainer(2, trainer2_1); // Train layer zero convolution with ct.
+
+		dnt.setTrainer(3, null); // No training for maxpool.
+
+		ContrastiveDivergenceTrainer trainer4_0 = new ContrastiveDivergenceTrainer();
+		ConvolutionalTrainer trainer4_1 = new ConvolutionalTrainer();
+		trainer0_1.operatorTrainer = trainer4_0;
+		dnt.setTrainer(4, trainer4_1); // Train layer zero convolution with ct.
+
+		return dnt;
+	}
+
+	@Override
+	public void start(Stage stage) {
+		DeepNetwork net = buildNetwork();
+		DeepNetworkTrainer dnt = buildTrainer(net);
+
+		// Set up UI
+		stage.setTitle("Aij Test UI");
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(pane, IMAGE_WIDTH, IMAGE_HEIGHT);
+		//ImageView exampleView = new ImageView(ImageTools.matrixToFXImage(Matrix.random(IMAGE_WIDTH, IMAGE_HEIGHT), true));
+		Button makeButton = new Button("Make Image");
+		//pane.add(exampleView, 0, 0);
+		pane.add(makeButton, 0, 0);
+		stage.setScene(scene);
+		stage.show();
 
 		// Load our training data.
 		System.out.println("Loading images.");
@@ -120,68 +175,10 @@ public class Main extends Application {
 
 		}
 
-		// Configure our trainers.
-		// We need a cdTrainer for the RBMs.
-		// We need a convTrainer to apply the CD trainer to the RBMs.
-		// Make some trainers.
-		ContrastiveDivergenceTrainer cdTrainer = new ContrastiveDivergenceTrainer();
-		cdTrainer.batchSize = 10;
-		cdTrainer.maxIterations = 100;
-		cdTrainer.learningRate = 0.1;
-		cdTrainer.gibbsSamples = 1;
-		cdTrainer.notificationIncrement = 10;
-		cdTrainer.regularization = 0.0001;
-
-		ConvolutionalTrainer convTrainer = new ConvolutionalTrainer();
-		convTrainer.operatorTrainer = cdTrainer;
-		convTrainer.subwindowsPerExample = 1000;
-		convTrainer.examplesPerBatch = 10;
-		convTrainer.maxIterations = 1000;
-
-		// Train first layer.
-		System.out.println("Examples loaded.  Training layer 1.");
-		convTrainer.train(conv0, examples, null, () -> {
-			System.out.println("Contrastive Divergence Training Error: " + cdTrainer.lastError);
-			System.out.println("RBM Energy: " + rbm0.getFreeEnergy());
-		});
-
-		// Save first layer.
-		//NetworkIOTools.saveNetworkToDisk(rbm0, "rbm0.model");
-
-		// Abstract the data by one level.
-		Matrix examples2 = conv1.predict(conv0.predict(examples));
-
-		// Train second layer.
-		System.out.println("Examples loaded.  Training layer 3.");
-		convTrainer.train(conv2, examples2, null, () -> {
-			System.out.println("Contrastive Divergence Training Error: " + cdTrainer.lastError);
-			System.out.println("RBM Energy: " + rbm1.getFreeEnergy());
-		});
-
-		// Abstract the data by one level.
-		Matrix examples3 = conv3.predict(conv2.predict(examples2));
-
-		// Train second layer.
-		System.out.println("Examples loaded.  Training layer 5.");
-		convTrainer.train(conv4, examples3, null, () -> {
-			System.out.println("Contrastive Divergence Training Error: " + cdTrainer.lastError);
-			System.out.println("RBM Energy: " + rbm2.getFreeEnergy());
-		});
-
 		// Now that we have all the input layers trained, let's try and abstract a representation for out positives.
-		Matrix temp = null;
-		temp = conv0.predict(positiveExamples);
-		temp = conv1.predict(temp);
-		temp = conv2.predict(temp);
-		temp = conv3.predict(temp);
-		Matrix positiveAbstraction = conv4.predict(temp);
-		temp = conv0.predict(negativeExamples);
-		temp = conv1.predict(temp);
-		temp = conv2.predict(temp);
-		temp = conv3.predict(temp);
-		Matrix negativeAbstraction = conv4.predict(temp);
 
 		// Concatenate our examples.  Positive on top.
+		/*
 		Matrix allAbstractions = Matrix.concatVertically(positiveAbstraction, negativeAbstraction);
 
 		// Make labels by setting the top part of the matrix to [1, 0] and the bottom to [0, 1]
@@ -192,43 +189,13 @@ public class Main extends Application {
 		for(int i=0; i < negativeAbstraction.numRows(); i++) {
 			labels.set(i+positiveAbstraction.numRows(), 1, 1.0);
 		}
-
-		// Train.
-		BackpropTrainer nnTrainer = new BackpropTrainer();
-		nnTrainer.batchSize = 10;
-		nnTrainer.learningRate = 0.1;
-		nnTrainer.maxIterations = 100000;
-		nnTrainer.notificationIncrement = 100;
-
-		/*
-		nnTrainer.train(fc, allAbstractions, labels, () -> {
-			System.out.println("NN Trainer Last Error: " + nnTrainer.lastError);
-		});
-
-		DeepNetwork dn = new DeepNetwork(conv0, conv1, conv2, conv3, conv4, fc);
-		*/
-		DeepNetwork dn = new DeepNetwork(conv0, conv1, conv2, conv3, conv4);
-		NetworkIOTools.saveNetworkToDisk(dn, FINAL_NETWORK_FILENAME);
-		return dn;
-	}
-
-	@Override
-	public void start(Stage stage) {
-		Network net = buildNetwork();
-
-		// Set up UI
-		stage.setTitle("Aij Test UI");
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
-		Scene scene = new Scene(pane, IMAGE_WIDTH, IMAGE_HEIGHT);
-		//ImageView exampleView = new ImageView(ImageTools.matrixToFXImage(Matrix.random(IMAGE_WIDTH, IMAGE_HEIGHT), true));
-		Button makeButton = new Button("Make Image");
-		//pane.add(exampleView, 0, 0);
-		pane.add(makeButton, 0, 0);
-		stage.setScene(scene);
-		stage.show();
+		 */
 
 		// Spawn a separate training thread.
+		Thread trainerThread = new Thread(() -> {
+
+		});
+
 		Thread makerThread = new Thread(() -> {
 			String filename = "output_"+ System.nanoTime() +".png";
 			Matrix mat = net.reconstruct(Matrix.random(1, net.getNumOutputs()));
